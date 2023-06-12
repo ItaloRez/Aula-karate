ebf9cc7b10361e66254932f7f655e41b62e80215
ItaloRez <italo.rezende@sharkdev.com.br>
2023-06-12 13:09:58 -0300

    aula 7

---
+++ aula_inatel/src/test/java/aula_inatel/json_placeholder/JsonRunner.java
@@ -0,0 +1,10 @@
+package aula_inatel.json_placeholder;
+
+import com.intuit.karate.junit5.Karate;
+
+class JsonRunner {
+    @Karate.Test
+    Karate testJsonPlaceholder() {
+        return Karate.run("json_placeholder").relativeTo(getClass());
+    }
+}
